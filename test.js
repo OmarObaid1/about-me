3d0172369dcd06c5cc342604792352d85c23c0bb
OmarObaid1 <omar.kamal28@outlook.com>
2021-08-02 16:03:53 +0300

    new

---
+++ test.js
@@ -0,0 +1,103 @@
+'use strict'
+
+alert("hello welcome to my website");
+
+
+let username = prompt("it is my name omar?");
+
+if (username !== null){
+    username = username.toLowerCase();
+    
+}
+
+    switch (username) {
+        case 'yes':
+            console.log('true answer');
+            alert("true answer");
+            break;
+
+        case 'no':
+            console.log('it\'s false answer');
+            alert("false answer");
+            break;
+
+            default:
+            alert("please answer yes or no ");
+    
+}
+ let age = prompt("is my age is 20?");
+ 
+ if (age !== null){
+    age = age.toLowerCase();
+}
+    switch (age) {
+        case 'yes':
+            console.log('true answer');
+            alert("true answer");
+            break;
+
+        case 'no':
+            console.log('it\'s false answer');
+            alert("false answer");
+            break;
+
+            default:
+            alert("please answer yes or no ");
+    }
+
+
+  let username2 = prompt("is my fav sport is football");
+  if ( username2 !== null){
+    username2 = username2.toLowerCase();
+}
+    switch (username2) {
+        case 'yes':
+            console.log('true answer');
+            alert("true answer");
+            break;
+
+        case 'no':
+            console.log('it\'s false answer');
+            alert("false answer");
+            break;
+
+            default:
+            alert("please answer yes or no ");
+    }
+let mansaf = prompt("is my fav food is mansaf");
+if ( mansaf !== null){
+    mansaf = mansaf.toLowerCase();
+}
+    switch (mansaf) {
+        case 'yes':
+            console.log('true answer');
+            alert("true answer");
+            break;
+
+        case 'no':
+            console.log('it\'s false answer');
+            alert("false answer");
+            break;
+
+            default:
+            alert("please answer yes or no ");
+    }
+    let ask = prompt("did i love this course :)");
+    if ( ask !== null){
+        mansaf = ask.toLowerCase();
+    }
+        switch (ask) {
+            case 'yes':
+                console.log('true answer');
+                alert("true answer");
+                break;
+    
+            case 'no':
+                console.log('it\'s false answer');
+                alert("false answer");
+                break;
+    
+                default:
+                alert("please answer yes or no ");
+        }
+  
