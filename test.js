795da8b8b49acf703fb6cf8eaeb516f1f2aa8623
OmarObaid1 <omar.kamal28@outlook.com>
2021-08-03 18:01:56 +0300

    new

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-'use strict'
+'use strict';
 
 alert("hello welcome to my website");
-
+let score =0;
+score+=1;
 
 let username = prompt("it is my name omar?");
 
@@ -14,6 +15,7 @@ if (username !== null){
         case 'yes':
             console.log('true answer');
             alert("true answer");
+            score+=1;
             break;
 
         case 'no':
@@ -34,6 +36,7 @@ if (username !== null){
         case 'yes':
             console.log('true answer');
             alert("true answer");
+            score+=1;
             break;
 
         case 'no':
@@ -54,6 +57,7 @@ if (username !== null){
         case 'yes':
             console.log('true answer');
             alert("true answer");
+            score+=1;
             break;
 
         case 'no':
@@ -72,6 +76,7 @@ if ( mansaf !== null){
         case 'yes':
             console.log('true answer');
             alert("true answer");
+            score+=1;
             break;
 
         case 'no':
@@ -84,12 +89,13 @@ if ( mansaf !== null){
     }
     let ask = prompt("did i love this course :)");
     if ( ask !== null){
-        mansaf = ask.toLowerCase();
+        ask = ask.toLowerCase();
     }
         switch (ask) {
             case 'yes':
                 console.log('true answer');
                 alert("true answer");
+                score+=1;
                 break;
     
             case 'no':
@@ -100,4 +106,86 @@ if ( mansaf !== null){
                 default:
                 alert("please answer yes or no ");
         }
+        alert("let'/s play a game can you guess a number i choies it");
+        let guessnumber ;
+
+        for ( let i=0 ; i<4 ;i++){
+        let guessnumber = Number(prompt('enter a guessnumber :  '));
+
+            if (guessnumber === 15) {
+
+                alert(" yes that's a correct answer");
+                console.log('yes that\'s a correct amswer' +guessnumber);
+                alert("correct answer");
+                score+=1;
+                break;
+            }else if (guessnumber > 15)
+               {
+                alert(" its too high");
+                alert("pick another number");
+                
+                
+
+            }else if (guessnumber < 15){
+
+                alert(" its too low");
+                alert("pick another number");
+                
+        
+            }
+            
+        }
+                alert("the correct number is 15");
+
+        
+        let place = ['lebnon','egypt','london','palstine','italy','germany','spain','iraq','kuwait','syria'];
+        // let travel = prompt("can you guess the country i would like to visit it");
+        
+        for(let x =0 ; x<6 ; x++){
+
+            let travel = prompt("can you guess the country i would like to visit it");
+            let y;
+        for ( y = 0; y<place.length ; y++){
+            
+                if (travel === place[y]){
+
+                    alert('correct answer');
+                    
+                    console.log('true i would like to visit this country ' + travel);
+                    score+=1;
+                    break;
+
+                }
+                // else if (travel !== place[y]){
+                //     alert('incorrect answer');
+                //     travel = prompt("can you guess the country i would like to visit it");
+                //     break;
+                // }
+            
+           
+            }
+            if(travel === place[y]){
+                break;
+
+            }else{
+                alert('incorrect');
+            }
+           
+        }
+        alert("that's a correct answer");
+        alert("scores is " + score + "out of 7");
+            
+
+            
+
+        
+            
+        
+        
+        
+        
+
+        
+        
+        
   
